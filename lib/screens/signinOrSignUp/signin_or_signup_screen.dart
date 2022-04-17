7349a4459d2101af05cbaec282b98801bd37302e
arora <aroramadhav65@gmail.com>
2022-04-17 15:36:48 +0530

    Update

--- lib/screens/signinOrSignUp/signin_or_signup_screen.dart
+++ lib/screens/signinOrSignUp/signin_or_signup_screen.dart
@@ -1,6 +1,6 @@
 import 'package:chat/components/primary_button.dart';
 import 'package:chat/constants.dart';
-import 'package:chat/screens/chats/chats_screen.dart';
+import 'package:chat_messaging_app_light_and_dark_theme/screens/chats/chats_screen.dart';
 import 'package:flutter/material.dart';
 
 class SigninOrSignupScreen extends StatelessWidget {
